07399cce665c75ca43ff3cb47cef54b18eedd0cb
Alan Dipert <alan@dipert.org>
2014-12-12 09:08:25 -0500

    port to boot2-rc1 aka tempdirs

--- build.boot
+++ build.boot
@@ -1,22 +1,22 @@
 (set-env!
- :src-paths    #{"src"}
- :dependencies '[[org.clojure/clojure       "1.6.0"       :scope "provided"]
-                 [boot/core                 "2.0.0-pre28" :scope "provided"]
-                 [tailrecursion/boot-useful "0.1.3"       :scope "test"]])
+ :source-paths #{"src"}
+ :dependencies '[[org.clojure/clojure "1.6.0"       :scope "provided"]
+                 [boot/core           "2.0.0-pre28" :scope "provided"]
+                 [adzerk/bootlaces    "0.1.4"       :scope "test"]])
 
-(require '[tailrecursion.boot-useful :refer :all]
-         '[adzerk.boot-test          :refer [test]])
+(require '[adzerk.bootlaces :refer :all]
+         '[adzerk.boot-test :refer [test]])
 
 (def +version+ "1.0.1")
 
-(useful! +version+)
+(bootlaces! +version+)
 
 (task-options!
- pom  [:project     'adzerk/boot-test
+ pom  {:project     'adzerk/boot-test
        :version     +version+
        :description "Run some tests in boot!"
        :url         "https://github.com/adzerk/boot-test"
        :scm         {:url "https://github.com/adzerk/boot-test"}
        :license     {:name "Eclipse Public License"
-                     :url  "http://www.eclipse.org/legal/epl-v10.html"}]
- test [:namespaces '#{adzerk.boot-test.test}])
+                     :url  "http://www.eclipse.org/legal/epl-v10.html"}}
+ test {:namespaces '#{adzerk.boot-test.test}})
--- src/adzerk/boot_test.clj
+++ src/adzerk/boot_test.clj
@@ -7,10 +7,9 @@
 (def pod-deps
   '[[pjstadig/humane-test-output "0.6.0" :exclusions [org.clojure/clojure]]])
 
-(defn next-pod! [pool]
-  (pool :refresh)
-  (doto (pool)
-    (pod/eval-in
+(defn init [fresh-pod]
+  (doto fresh-pod
+    (pod/with-eval-in
      (require '[clojure.test :as t]
               '[pjstadig.humane-test-output :refer [activate!]])
      (activate!)
@@ -22,17 +21,22 @@
            (t/do-report {:type :end-test-ns :ns ns-obj}))
          @t/*report-counters*)))))
 
+;;; This prevents a name collision WARNING between the test task and
+;;; clojure.core/test, a function that nobody really uses or cares
+;;; about.
+(if ((loaded-libs) 'boot.user)
+  (ns-unmap 'boot.user 'test))
+
 (core/deftask test
   "Run clojure.test tests in a pod."
   [n namespaces NAMESPACE #{sym} "Symbols of the namespaces to run tests in."
    f filters EXPR #{any} "Clojure expressions that are evaluated with % bound to a Var in a namespace under test. All must evaluate to true for a Var to be considered for testing by clojure.test/test-vars."]
-  (let [worker-pods (pod/pod-pool 2 (update-in (core/get-env) [:dependencies] into pod-deps))]
+  (let [worker-pods (pod/pod-pool (update-in (core/get-env) [:dependencies] into pod-deps) :init init)]
     (core/cleanup (worker-pods :shutdown))
-    (core/with-pre-wrap
+    (core/with-pre-wrap fileset
       (if (seq namespaces)
-        (let [pod (next-pod! worker-pods)
-              filterf `(~'fn [~'%] (and ~@filters))
-              summary (pod/eval-in pod
+        (let [filterf `(~'fn [~'%] (and ~@filters))
+              summary (pod/with-eval-in (worker-pods :refresh)
                         (doseq [ns '~namespaces] (require ns))
                         (let [ns-results (map (partial test-ns* ~filterf) '~namespaces)]
                           (-> (reduce (partial merge-with +) ns-results)
@@ -40,4 +44,5 @@
                               (doto t/do-report))))]
           (when (> (apply + (map summary [:fail :error])) 0)
             (throw (ex-info "Some tests failed or errored" summary))))
-        (println "No namespaces were tested.")))))
+        (println "No namespaces were tested."))
+      fileset)))
