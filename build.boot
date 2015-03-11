0d01e3444acd3b0d1c9dbffad32021178a1fbf03
Micha Niskin <micha.niskin@gmail.com>
2015-03-10 22:59:48 -0400

    Test all namespaces if --namespaces not given (fixes #4)

--- build.boot
+++ build.boot
@@ -1,13 +1,13 @@
 (set-env!
  :source-paths #{"src"}
- :dependencies '[[org.clojure/clojure "1.6.0"     :scope "provided"]
-                 [boot/core           "2.0.0-rc1" :scope "provided"]
-                 [adzerk/bootlaces    "0.1.4"     :scope "test"]])
+ :dependencies '[[org.clojure/clojure "1.6.0"      :scope "provided"]
+                 [boot/core           "2.0.0-rc12" :scope "provided"]
+                 [adzerk/bootlaces    "0.1.11"     :scope "test"]])
 
 (require '[adzerk.bootlaces :refer :all]
          '[adzerk.boot-test :refer [test]])
 
-(def +version+ "1.0.2")
+(def +version+ "1.0.4")
 
 (bootlaces! +version+)
 
@@ -17,6 +17,6 @@
        :description "Run some tests in boot!"
        :url         "https://github.com/adzerk/boot-test"
        :scm         {:url "https://github.com/adzerk/boot-test"}
-       :license     {:name "Eclipse Public License"
-                     :url  "http://www.eclipse.org/legal/epl-v10.html"}}
+       :license     {"Eclipse Public License"
+                     "http://www.eclipse.org/legal/epl-v10.html"}}
  test {:namespaces '#{adzerk.boot-test.test}})
--- src/adzerk/boot_test.clj
+++ src/adzerk/boot_test.clj
@@ -5,14 +5,19 @@
             [boot.core :as core]))
 
 (def pod-deps
-  '[[pjstadig/humane-test-output "0.6.0" :exclusions [org.clojure/clojure]]])
+  '[[org.clojure/tools.namespace "0.2.10" :exclusions [org.clojure/clojure]]
+    [pjstadig/humane-test-output "0.6.0"  :exclusions [org.clojure/clojure]]])
 
 (defn init [fresh-pod]
   (doto fresh-pod
     (pod/with-eval-in
      (require '[clojure.test :as t]
-              '[pjstadig.humane-test-output :refer [activate!]])
+              '[clojure.java.io :as io]
+              '[pjstadig.humane-test-output :refer [activate!]]
+              '[clojure.tools.namespace.find :refer [find-namespaces-in-dir]])
      (activate!)
+     (defn all-ns* [& dirs]
+       (mapcat #(find-namespaces-in-dir (io/file %)) dirs))
      (defn test-ns* [pred ns]
        (binding [t/*report-counters* (ref t/*initial-report-counters*)]
          (let [ns-obj (the-ns ns)]
@@ -28,21 +33,36 @@
   (ns-unmap 'boot.user 'test))
 
 (core/deftask test
-  "Run clojure.test tests in a pod."
-  [n namespaces NAMESPACE #{sym} "Symbols of the namespaces to run tests in."
-   f filters EXPR #{any} "Clojure expressions that are evaluated with % bound to a Var in a namespace under test. All must evaluate to true for a Var to be considered for testing by clojure.test/test-vars."]
+  "Run clojure.test tests in a pod.
+
+  The --namespaces option specifies the namespaces to test. The default is to
+  run tests in all namespaces found in the project.
+
+  The --filters option specifies Clojure expressions that are evaluated with %
+  bound to a Var in a namespace under test. All must evaluate to true for a Var
+  to be considered for testing by clojure.test/test-vars."
+
+  [n namespaces NAMESPACE #{sym} "The set of namespace symbols to run tests in."
+   f filters    EXPR      #{edn} "The set of expressions to use to filter namespaces."]
+
   (let [worker-pods (pod/pod-pool (update-in (core/get-env) [:dependencies] into pod-deps) :init init)]
     (core/cleanup (worker-pods :shutdown))
     (core/with-pre-wrap fileset
-      (if (seq namespaces)
-        (let [filterf `(~'fn [~'%] (and ~@filters))
-              summary (pod/with-eval-in (worker-pods :refresh)
-                        (doseq [ns '~namespaces] (require ns))
-                        (let [ns-results (map (partial test-ns* ~filterf) '~namespaces)]
-                          (-> (reduce (partial merge-with +) ns-results)
-                              (assoc :type :summary)
-                              (doto t/do-report))))]
-          (when (> (apply + (map summary [:fail :error])) 0)
-            (throw (ex-info "Some tests failed or errored" summary))))
-        (println "No namespaces were tested."))
-      fileset)))
+      (let [worker-pod (worker-pods :refresh)
+            namespaces (or (seq namespaces)
+                           (pod/with-eval-in worker-pod
+                             (all-ns* ~@(->> fileset
+                                             core/input-dirs
+                                             (map (memfn getPath))))))]
+        (if (seq namespaces)
+          (let [filterf `(~'fn [~'%] (and ~@filters))
+                summary (pod/with-eval-in worker-pod
+                          (doseq [ns '~namespaces] (require ns))
+                          (let [ns-results (map (partial test-ns* ~filterf) '~namespaces)]
+                            (-> (reduce (partial merge-with +) ns-results)
+                                (assoc :type :summary)
+                                (doto t/do-report))))]
+            (when (> (apply + (map summary [:fail :error])) 0)
+              (throw (ex-info "Some tests failed or errored" summary))))
+          (println "No namespaces were tested."))
+        fileset))))
